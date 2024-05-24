318c2c1846fa1f711706a89c197dbdf4c7c9462c
Robin Gagnon <me@reobin.dev>
2024-05-24 15:59:29 -0500

    docs: Add README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # vimcolorschemes/extractor.nvim
+
+This is a simple plugin to extract color groups from a neovim buffer. It's used
+in the context of [vimcolorschemes.com](https://vimcolorschemes.com) to extract
+color data from a vim colorscheme and generate a preview.
+
+## Requirements
+
+* vim syntax highlighting (not treesitter)
+
+## Installation
+
+```lua
+-- lazy.nvim
+{
+  "vimcolorschemes/extractor.nvim"
+}
+```
+
+## Usage
+
+Returns a lua table with the color groups found in the buffer + some extra.
+
+`:Extractor`
--- lua/extractor/init.lua
+++ lua/extractor/init.lua
@@ -11,12 +11,14 @@ function M.run()
   local normal_bg_color_value = Vim.get_color_group_value("Normal", "bg#") or "#000000"
 
   local color_groups = { { name = "Normal", fg = normal_fg_color_value, bg = normal_bg_color_value } }
+
   for _, color_group_name in ipairs(color_group_names) do
     local fg_color_value = Vim.get_color_group_value(color_group_name, "fg#") or normal_fg_color_value
     local bg_color_value = Vim.get_color_group_value(color_group_name, "bg#") or normal_bg_color_value
     local color_group = { name = color_group_name, fg = fg_color_value, bg = bg_color_value }
     table.insert(color_groups, color_group)
   end
+
   return color_groups
 end
 
